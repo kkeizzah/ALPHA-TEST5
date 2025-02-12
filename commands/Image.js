6abd1a8f204617d510976b80a354506c27ffa375
kkeizzah <keizzahketh@gmail.com>
2025-02-12 23:08:38 +0300

    Update Image.js

--- commands/Image.js
+++ commands/Image.js
@@ -4,6 +4,39 @@ const axios = require('axios');
 const conf = require(__dirname + '/../set');
 
 keith({
+  nomCom: "screenshot",
+  aliases: ["ss", "sshot"],
+  categorie: "AI",
+  reaction: "📽️"
+}, async (dest, zk, commandeOptions) => {
+  const { repondre, ms, arg } = commandeOptions;
+
+  try {
+    // Caption for the screenshot
+    const cap = `*Screenshot by Alpha Md*`;
+
+    // Check if a URL is provided
+    if (!arg[0]) {
+      return repondre('Please insert a website link to take a screenshot!');
+    }
+
+    // Construct the screenshot URL
+    const image = `https://image.thum.io/get/fullpage/${arg[0]}`;
+
+    // Send the screenshot image with the caption
+    await zk.sendMessage(dest, {
+      image: { url: image },
+      caption: cap
+    }, { quoted: ms });
+
+  } catch (error) {
+    // Log the error and notify the user with a formatted error message
+    console.error(error);
+    repondre(`An error occurred while processing the screenshot: ${error.message}`);
+  }
+});
+
+keith({
   nomCom: "img",
   aliases: ["image", "images"],
   categorie: "Images",
