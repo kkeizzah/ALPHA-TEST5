f71a6c3cc79cf26ffcb88cdd2f8a6b6d8e6d4e7d
kkeizzah <keizzahketh@gmail.com>
2025-02-14 07:46:01 +0300

    Update Mods.js

--- commands/Mods.js
+++ commands/Mods.js
@@ -1,5 +1,6 @@
 const { keith } = require('../keizzah/keith');
 const axios = require("axios");
+const fancy = require("../keizzah/style");
 let { Sticker, createSticker, StickerTypes } = require('wa-sticker-formatter');
 const { isUserBanned, addUserToBanList, removeUserFromBanList } = require("../bdd/banUser");
 const { addGroupToBanList, isGroupBanned, removeGroupFromBanList } = require("../bdd/banGroup");
@@ -571,3 +572,26 @@ keith({ nomCom: "vv2", aliases: ["send2", "keep2"], categorie: "Mods" }, async (
 
   })
 ;
+
+
+keith({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+    const { arg, repondre, prefixe } = commandeOptions;
+    const id = arg[0]?.match(/\d+/)?.join('');
+    const text = arg.slice(1).join(" ");
+
+    try {
+        if (id === undefined || text === undefined) {
+            return await repondre(`\nExample : ${prefixe}fancy 10 Alpha-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('ALPHA-MD', fancy));
+        }
+
+        const selectedStyle = fancy[parseInt(id) - 1];
+        if (selectedStyle) {
+            return await repondre(fancy.apply(selectedStyle, text));
+        } else {
+            return await repondre('_Style introuvable :(_');
+        }
+    } catch (error) {
+        console.error(error);
+        return await repondre('_Une erreur s\'est produite :(_');
+    }
+});
